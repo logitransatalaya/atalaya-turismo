ea8da31c874440bc7959c8f970157109823809ab
Camiloatalaya <developeratalaya2@outlook.com>
2021-10-05 17:46:25 -0500

    The section of why watchtower was added, with the cards and icons

---
+++ frontend/src/components/HotelFeatures/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const HotelFeatures = () => {
+	return (
+		<div>
+			<div>
+				<h1>Aqui van las caracteristicas de el hotel </h1>
+			</div>
+			<hr />
+			<div>
+				<h1>SERVICIOS MAS POPULARES </h1>
+			</div>
+			<hr />
+			<div>
+				<h1>SERVICIOS MAS POPULARES </h1>
+			</div>
+			<hr />
+			<div>
+				<h1>HABITACIONES DEL HOTEL </h1>
+			</div>
+			<hr />
+			<div>
+				<h1>SOBRE EL HOTEL TRYP MEDELLIN </h1>
+			</div>
+		</div>
+	)
+}
