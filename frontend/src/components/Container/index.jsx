7c7b8a41fd8e1ea88d6d8deb561a3974a55a8369
Jarrison Cano <developeratalaya1@outlook.com>
2021-10-04 19:03:26 -0500

    agregando el container y el navbar

--- frontend/src/components/Container/index.jsx
+++ frontend/src/components/Container/index.jsx
@@ -1,10 +1,10 @@
+import './styles.css'
 import React from 'react'
-import styled from 'styles/Container.module.css'
 
 export const Container = ({ children }) => {
 	return (
 		<div>
-			<div className={styled.container}>{children}</div>
+			<div className='Container'>{children}</div>
 		</div>
 	)
 }
--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,44 +1,55 @@
 import './styles.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Container } from 'components/Container'
 
 export const NavBar = () => {
 	return (
-		<nav className='navBar-container'>
-			<span className='logoContainer'>
-				<img src='/logo.png' alt='' />
-			</span>
-			<div className='iconsContainer'>
-				<a className='loginIcon' href='/'>
-					<img src='/home/user-navbar.png' alt='' />
-				</a>
-				<a className='burgerIcon' href='/'>
-					<img src='/home/burgerMenu-navbar.png' alt='' />
-				</a>
-			</div>
-			<ul className='navbarList'>
-				<li>
-					<Link to='/'>INICIO</Link>
-				</li>
-				<li>
-					<Link to='/hoteles'>HOTELES</Link>{' '}
-				</li>
-				<li>
-					<Link to='/transporte'>TRANSPORTE</Link>
-				</li>
-				<li>
-					<Link to='/planes'>PLANES</Link>
-				</li>
-				<li>
-					<Link to='/tours'>TOURS</Link>
-				</li>
-				<li>
-					<Link to='/ofertas'>OFERTAS</Link>
-				</li>
-				<li>
-					<Link to='/medios-de-pago'>MEDIOS DE PAGO</Link>
-				</li>
-			</ul>
-		</nav>
+		<div className='navBar-background'>
+			<Container>
+				<nav className='navBar-container'>
+					<span className='logoContainer'>
+						<img src='/logo.png' alt='' />
+					</span>
+					<div className='iconsContainer'>
+						<a className='loginIcon' href='/'>
+							<img src='/home/user-navbar.png' alt='' />
+						</a>
+						<a className='burgerIcon' href='/'>
+							<img src='/home/burgerMenu-navbar.png' alt='' />
+						</a>
+					</div>
+					<div className='navbarDesktop'>
+						<div className='navbarButtonsContainer'>
+							<button>REGISTRATE</button>
+							<button>INICIA SESIÓN</button>
+						</div>
+						<ul className='navbarList'>
+							<li>
+								<Link to='/'>INICIO</Link>
+							</li>
+							<li>
+								<Link to='/hoteles'>HOTELES</Link>{' '}
+							</li>
+							<li>
+								<Link to='/transporte'>TRANSPORTE</Link>
+							</li>
+							<li>
+								<Link to='/planes'>PLANES</Link>
+							</li>
+							<li>
+								<Link to='/tours'>TOURS</Link>
+							</li>
+							<li>
+								<Link to='/ofertas'>OFERTAS</Link>
+							</li>
+							<li>
+								<Link to='/medios-de-pago'>MEDIOS DE PAGO</Link>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</Container>
+		</div>
 	)
 }
