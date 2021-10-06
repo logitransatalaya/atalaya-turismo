ab819f36cb72976033979e23270af3aef4012ce7
Fabian Pineda Salazar <developeratalata0@outlook.es>
2021-10-05 20:04:58 -0500

    Setting Embedded Models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+n=ovv^(qj6yrw9m7_h#gvwo0!-9bq5kg(i28hfy+e1ey1vdn+'
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'hotels'
 ]
 
 MIDDLEWARE = [
