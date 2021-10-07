f9c512704061675fa81e13b9e00efb66866035d0
developer4 <developeratalaya4@gmail.com>
2021-10-07 13:18:08 -0500

    disappear buttons

--- frontend/src/components/FloatingButtons/styles.js
+++ frontend/src/components/FloatingButtons/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const ContainerBtns = styled.div`
 	display: inline-block;
@@ -6,12 +6,42 @@ export const ContainerBtns = styled.div`
 	top: 160px;
 	right: 0;
 	z-index: 11111;
+
+	@media (max-width: 600px) {
+		top: 60px;
+	}
 `
+
+const disappear = keyframes`
+	from {
+		opacity: 1;
+	}
+	50% {
+		opacity: 0.5;
+	}
+	to {
+		opacity: 0;
+		visibility: hidden;
+	}
+`
+
 export const Button = styled.div`
 	width: 55px;
 	height: 55px;
 	cursor: pointer;
 	transition: all 0.53s ease;
+
+	.red {
+		background: red;
+		width: 100%;
+		height: 100%;
+	}
+
+	.blue {
+		background: blue;
+		width: 100%;
+		height: 100%;
+	}
 
 	.acontainer {
 		width: 100%;
@@ -21,8 +51,6 @@ export const Button = styled.div`
 
 	.tiptextW,
 	.tiptextM {
-		visibility: ${(props) =>
-			props.showTooltip === false ? `hidden` : `visible`};
 		width: max-content;
 		color: #fff;
 		text-align: center;
@@ -50,7 +78,23 @@ export const Button = styled.div`
 		top: 40%;
 	}
 
+	.animation {
+		transition: all 0.5s ease;
+		animation: ${disappear} 0.5s ease forwards;
+	}
+
+	.icons {
+		opacity: 0.8;
+
+		border-radius: 15px 0 0 15px;
+	}
+
 	&:hover {
 		transform: translateX(-10px);
+	}
+
+	@media (max-width: 600px) {
+		width: 45px;
+		height: 45px;
 	}
 `
