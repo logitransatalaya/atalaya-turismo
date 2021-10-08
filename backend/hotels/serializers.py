42cee1b225c5e45fec4a30e389ce7c0f031573b1
Fabian Pineda Salazar <developeratalata0@outlook.es>
2021-10-07 19:48:16 -0500

    Set some views

--- backend/hotels/serializers.py
+++ backend/hotels/serializers.py
@@ -2,7 +2,7 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import National_city
 
-class National_citySerializer(serializers.ModelSerializer):
+class National_city_serializer(serializers.ModelSerializer):
     
     class Meta:
         model = National_city
---
+++ backend/hotels/views.py
@@ -0,0 +1,25 @@
+from django.db.models import query
+from django.shortcuts import render
+from .models import National_city
+from rest_framework import generics
+from .serializers import National_city_serializer
+from backend.hotels import serializers
+
+serializer_class = National_city_serializer
+
+
+class National_city_serializer(generics.CreateAPIView):
+    # API endpoint that allows creation of a new city
+    queryset = National_city.objects.all()
+    serializers = National_city_serializer
+
+
+class National_city_list(generics.ListAPIView):
+    # API endpoint that allows customer to be viewed
+    queryset = National_city.objects.all()
+    serializers_class = National_city_serializer
+
+class National_city_detail(generics.RetrieveAPIView):
+    # API endpoint that return a single city by pk
+    queryset = National_city.objects.all()
+    serializers_class = 
