3488a2c5584f4d1ec08a44ec097d71164019d27e
Jarrison Cano <developeratalaya1@outlook.com>
2021-10-09 11:27:28 -0500

    agregando el componente GoBack

--- frontend/src/components/GlobalComponents/GoBack/index.jsx
+++ frontend/src/components/GlobalComponents/GoBack/index.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import { useHistory } from 'react-router'
 
 export const GoBack = () => {
-	const history = useHistory()
+	const { goBack } = useHistory()
 
-	const handleClick = () => {
-		console.log(history)
-	}
+	return (
+		<div className='goback-container' style={{ width: '2.5rem' }}>
+			<img onClick={goBack} src='/goBack.svg' alt='go back' />
+			<style jsx>
+				{`
+					.goback-container img {
+						width: 100%;
+						cursor: pointer;
+					}
 
-	return <div onClick={handleClick}>volver atras</div>
+					.goback-container img:hover {
+						opacity: 0.7;
+					}
+				`}
+			</style>
+		</div>
+	)
 }
