974d35cf8528122a5f054be6cd4f3b06fefa96c4
brayanAtalaya <developeratalaya3@outlook.com>
2021-10-12 15:52:58 -0500

    fix: arreglando bug en el componente de Health, la parte del banner en escritorio y movil

--- frontend/src/components/Health/HealthHeader/styles.js
+++ frontend/src/components/Health/HealthHeader/styles.js
@@ -1,15 +1,21 @@
 import styled from 'styled-components'
-import bannerSalud from 'images/bannerSalud.png'
+import bannerSaludD from 'images/bannerSalud.png'
+import bannerSaludM from 'images/bannerSaludM.png'
 
 export const HealthHeaderContainer = styled.div`
 	.contentBanner {
-		background-image: url(${bannerSalud});
+		background-image: url(${bannerSaludD});
 		width: 100%;
-		height: 585px;
+		height: 600px;
 		background-repeat: no-repeat;
-		background-size: contain;
-		background-position: right;
-		background-attachment: fixed;
+		background-size: cover;
+		background-position: center;
 		margin-bottom: 2em;
+
+		@media (max-width: 600px) {
+			background-image: url(${bannerSaludM});
+			height: 250px;
+			margin-bottom: 3em;
+		}
 	}
 `
