368df365122a8aec7f7bd2681a0b4df09890581b
Camiloatalaya <developeratalaya2@outlook.com>
2021-10-13 09:12:39 -0500

    addin heigh to the seccion image-background to component HealthNotes

--- frontend/src/components/Health/HealthNotes/styles.js
+++ frontend/src/components/Health/HealthNotes/styles.js
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 export const Boximgsmall = styled.div`
 	img {
 		width: 100%;
+		height: 100%;
 	}
-
+	height: 200px;
 	@media (min-width: 600px) {
 		display: none;
 	}
