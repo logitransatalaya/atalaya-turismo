a0392e8c68da8957287194a8b5008dee956a64e0
developer4 <developeratalaya4@gmail.com>
2021-10-14 17:09:57 -0500

    sincronizacion con la DB

--- backend/hotels/views.py
+++ backend/hotels/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import serializers
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
