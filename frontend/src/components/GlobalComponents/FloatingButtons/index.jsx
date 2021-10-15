285f769c68bbe26dcc7ee10a6f5830882f7c6b4d
brayanAtalaya <developeratalaya3@outlook.com>
2021-10-15 13:45:37 -0500

    fix: limpiando la consola de warnings

--- frontend/src/components/GlobalComponents/FloatingButtons/index.jsx
+++ frontend/src/components/GlobalComponents/FloatingButtons/index.jsx
@@ -45,6 +45,7 @@ const FloatingButtons = () => {
 				messageWsp: `Deseo%20reservar%20el%20plan%20a%20${currentMessage.title}`
 			})
 		}
+		// eslint-disable-next-line
 	}, [currentMessage])
 
 	useEffect(() => {
