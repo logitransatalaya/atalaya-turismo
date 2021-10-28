37493d6b7b5d0476f3b22b8dae67c57270aede85
brayanAtalaya <developeratalaya3@outlook.com>
2021-10-28 15:10:13 -0500

    subiendo los cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
