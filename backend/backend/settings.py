e0b4470c504ee4e7e002e7319cf13470237f4917
fabianAtalaya <developeratalaya0@gmail.com>
2021-11-05 15:22:00 -0500

    setted settings to deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+import os 
+import sys
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+n=ovv^(qj6yrw9m7_h#gvwo0!-9bq5kg(i28hfy+e1ey1vdn+'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == True
 
-ALLOWED_HOSTS = []
+DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', 'False') == 'True'
+
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOST', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
@@ -89,18 +95,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'turismo',
-        'USER': 'atalaya',
-        'PASSWORD': 'atalaya1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
+if DEVELOPMENT_MODE is True:
+    DATABASE  = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
-
+elif len(sys.argv) > 0 and sys.agrv[1] != 'collectstatic':
+    if os.getenv('DATABASE_URL', None) is None:
+        raise Exception('DATABASE_URL enviroment varibale not defined')
+    DATA = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -142,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
