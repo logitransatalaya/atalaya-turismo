747295f4d9da711545ae800f85af62b6e1b655de
Camiloatalaya <developeratalaya2@outlook.com>
2021-11-10 08:57:12 -0500

    feat: Agregando el componente loader para mostrar antes de que la informacion cargue en el componente offers

--- frontend/src/components/Home/HomeOffersFlash/index.jsx
+++ frontend/src/components/Home/HomeOffersFlash/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { OffersCard } from '../OffersCard'
+import { OffersCard } from './OffersCard'
 import { splideOptionsOffers } from 'lib/splide'
 import Splide from '@splidejs/react-splide/dist/js/components/Splide'
 import SplideSlide from '@splidejs/react-splide/dist/js/components/SplideSlide'
@@ -8,30 +8,6 @@ export const HomeOffersFlash = ({ image }) => {
 	return (
 		<div className='recomendedCard-container'>
 			<Splide options={splideOptionsOffers}>
-				<SplideSlide>
-					<OffersCard image={image} title={'CANCUN'} />
-				</SplideSlide>
-				<SplideSlide>
-					<OffersCard image={image} title={'MEXICO'} />
-				</SplideSlide>
-				<SplideSlide>
-					<OffersCard image={image} title={'COLOMBIA'} />
-				</SplideSlide>
-				<SplideSlide>
-					<OffersCard image={image} title={'CHILE'} />
-				</SplideSlide>
-				<SplideSlide>
-					<OffersCard image={image} title={'CANCUN'} />
-				</SplideSlide>
-				<SplideSlide>
-					<OffersCard image={image} title={'MEXICO'} />
-				</SplideSlide>
-				<SplideSlide>
-					<OffersCard image={image} title={'COLOMBIA'} />
-				</SplideSlide>
-				<SplideSlide>
-					<OffersCard image={image} title={'CHILE'} />
-				</SplideSlide>
 				<SplideSlide>
 					<OffersCard image={image} title={'CANCUN'} />
 				</SplideSlide>
