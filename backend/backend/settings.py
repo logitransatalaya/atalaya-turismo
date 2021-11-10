5a38468cee31d2a942075b18a9f76c98b713bdbc
Yeison Serna <yeiserna@gmail.com>
2021-11-10 16:30:38 -0500

    add config for static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,8 +92,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'django.middleware.common.CommonMiddleware'
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.conf.urls import url
+from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import include
+from django.views.static import serve
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':
+    settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':
+    settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('api/', include('hotels.urls')),
     path('api/', include('plans.urls')),
