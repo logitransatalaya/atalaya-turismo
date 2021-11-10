f677ec9876cde5f7383ddee58a9b9cae6fe52532
Yeison Serna <yeiserna@gmail.com>
2021-11-10 15:29:01 -0500

    drop warning with the offersflash

--- backend/offersflash/models.py
+++ backend/offersflash/models.py
@@ -7,5 +7,5 @@ class Offersflash(models.Model):
     """ Modelo no includes """
     url_img = models.URLField(max_length=250)
     title = models.CharField(max_length=250)
-    cop = models.BigIntegerField(max_length=250)
+    cop = models.BigIntegerField()
 
