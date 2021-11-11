1822adcbe244fc50fa46d4e041e9543a3775c25c
Camiloatalaya <developeratalaya2@outlook.com>
2021-11-11 15:42:40 -0500

    cambio de nombre a la carpeta de nevgaci

--- frontend/src/components/GlobalComponents/NavBar/styles.js
+++ frontend/src/components/GlobalComponents/NavBar/styles.js
@@ -1,190 +1,122 @@
 import styled from 'styled-components'
 
-export const NavbarStyled = styled.div`
-	background-color: #10216f;
-	position: sticky;
-	top: 0;
-	left: 0;
-	width: 100%;
-	z-index: 111;
-	.navBar-container {
+export const StyleNavbarBrayan = styled.div`
+	background-color: #0f216f;
+
+	.navbar {
+		height: 130px;
 		display: flex;
 		justify-content: space-between;
-		padding: 0.3rem 1rem;
-		position: relative;
-	}
-	.logoContainer {
 		align-items: center;
-		display: flex;
-		justify-content: center;
-		width: 20%;
-		img {
-			width: 100%;
-		}
-	}
-	.logoContainerMenu {
-		align-items: center;
-		display: flex;
-		justify-content: center;
-		width: 100%;
-		img {
+
+		.box_imgLogo {
 			width: 150px;
-		}
-	}
-	.iconsContainer {
-		align-items: center;
-		display: flex;
-		justify-content: space-between;
-		gap: 1rem;
-		width: 25%;
-		.loginIcon {
-			align-items: center;
-			display: flex;
-			justify-content: center;
-			height: 50%;
-			width: 100%;
+
 			img {
 				width: 100%;
 			}
 		}
-		.burgerIcon {
-			align-items: center;
+
+		.content_options {
 			display: flex;
-			height: 50%;
-			justify-content: center;
-			width: 100%;
-			img {
-				width: 100%;
-			}
-		}
-	}
-	.navbarDesktop {
-		align-items: flex-start;
-		display: flex;
-		background-color: #10216f;
-		flex-direction: column-reverse;
-		gap: 1rem;
-		min-height: 100vh;
-		justify-content: flex-end;
-		left: 0;
-
-		padding: 1rem 0;
-		padding-left: 2.5rem;
-		position: fixed;
-		transform: translateX(-105%);
-
-		transition: ease-in-out 0.3s;
-		top: 0;
-		width: calc(70% - 2rem);
-		z-index: 1000;
-	}
-
-	.navbarList {
-		display: flex;
-		flex-direction: column;
-		gap: 1.5rem;
-		margin: 2rem 0;
-
-		padding: 0;
-
-		li {
-			list-style: none;
-		}
-
-		a {
-			color: #f4a719;
-			font-weight: 500;
-			text-decoration: none;
-
-			&:hover {
-				opacity: 0.8;
-			}
-		}
-	}
-
-	.navbarButtonsContainer {
-		color: white;
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-
-		button {
-			border: none;
-			border-radius: 3px;
-			color: white;
-			padding: 0.5rem 1rem;
-
-			&:hover {
-				opacity: 0.8;
-			}
-
-			&:nth-of-type(1) {
-				background-color: #0a1744;
-			}
-			&:nth-of-type(2) {
-				background-color: #f4a719;
-			}
-		}
-	}
-
-	@media (min-width: 550px) {
-		.logoContainer {
-			padding: 0.5rem;
-			width: 5rem;
-		}
-		.iconsContainer {
-			align-items: center;
-			display: flex;
-			justify-content: space-between;
-			gap: 1rem;
-
-			width: 7rem;
-		}
-
-		.navbarDesktop {
-			font-size: 1.3rem;
-			width: calc(70% - 15rem);
-		}
-	}
-	@media (min-width: 950px) {
-		.logoContainer {
-			padding: 0.5rem;
-			width: 8rem;
-		}
-
-		.navbarDesktop {
-			align-items: flex-end;
 			flex-direction: column;
-			font-size: 1rem;
-			gap: 2rem;
+			justify-content: flex-end;
+			align-items: center;
+			height: 100%;
+			margin-bottom: 25px;
+			margin-right: 50px;
 
-			min-height: auto;
-			justify-content: center;
-			margin: 0.5rem 0;
-			padding-left: 0;
+			.navigation {
+				display: flex;
+				list-style: none;
 
-			position: initial;
-			transform: none;
-			width: auto;
+				li {
+					margin-right: 1em;
 
-			.navbarButtonsContainer {
-				color: white;
-				flex-direction: row;
-				gap: 1rem;
+					&:hover {
+						opacity: 0.8;
+					}
+
+					a {
+						text-decoration: none;
+						color: orange;
+						font-weight: 400;
+						font-size: 22px;
+					}
+				}
 			}
 		}
 
-		.iconsContainer {
-			display: none;
-		}
+		@media (max-width: 600px) {
+			background-color: #0f216f;
+			height: 100vh;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			width: 70%;
+			position: fixed;
+			top: 0;
+			left: 0;
+			bottom: 0;
+			z-index: 10000;
+			box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175);
+			margin-left: -100%;
+			transition: margin-left 0.5s;
 
-		.navbarList {
-			margin: 0;
-			flex-direction: row;
+			.content_options {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				height: 50%;
+
+				.search {
+					align-items: center;
+					display: flex;
+					text-align: center;
+					justify-content: center;
+					padding-top: 0;
+
+					input {
+						width: 80%;
+						border: none;
+						padding: 5px 10px;
+						border-radius: 1em;
+					}
+				}
+
+				.navigation {
+					flex-direction: column;
+					padding: 0;
+					align-items: center;
+
+					li {
+						margin: 0;
+						margin-top: 1em;
+
+						a {
+							text-decoration: none;
+							color: orange;
+							font-size: 19px;
+						}
+					}
+				}
+			}
 		}
 	}
 
-	.navbarVisible {
-		transform: translateX(0);
+	.navbar_active {
+		margin-left: 0;
+	}
+`
+export const StyleButton = styled.div`
+	position: fixed;
+	top: 1em;
+	right: 1em;
+	display: none;
+	z-index: 1000;
+
+	@media (max-width: 600px) {
+		display: block;
 	}
 `
