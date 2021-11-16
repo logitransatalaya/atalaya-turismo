129110e30908183f839b3d3e0bc8b18b4f1b7d45
Camiloatalaya <developeratalaya2@outlook.com>
2021-11-16 16:06:16 -0500

    feat: Agregando las imagenes para banner movil en el componte de home

--- frontend/src/components/Home/HomeSlider/index.jsx
+++ frontend/src/components/Home/HomeSlider/index.jsx
@@ -1,44 +1,57 @@
 import { Slider } from './styles'
 import { splideOptionsLoop } from 'lib/splide'
 import React, { useEffect, useState } from 'react'
+// movil //
+import slideDefault11 from '../../../images/caño-movil.jpg'
+import slideDefault44 from '../../../images/guatape-movil.jpg'
+import slideDefault33 from '../../../images/mina-sal-movil.jpg'
+import slideDefault66 from '../../../images/desierto-movil.jpg'
+import slideDefault00 from '../../../images/destinos-movil.jpg'
+import slideDefault22 from '../../../images/san-andres-movil.jpg'
+// web //
 import slideDefault1 from '../../../images/caño.png'
 import slideDefault3 from '../../../images/guatape.png'
 import slideDefault4 from '../../../images/desierto.png'
-import slideDefault from '../../../images/caño-movil.jpg'
 import slideDefault5 from '../../../images/cartagena.png'
 import slideDefault2 from '../../../images/san-andres.png'
 import slideDefault6 from '../../../images/mina-de-sal.png'
+import slideDefault0 from '../../../images/destinos-web.jpg'
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 
 const arrayInfo = [
 	{
+		url: slideDefault0,
+		urlMovil: slideDefault00,
+		alt: 'mas destacados'
+	},
+	{
 		url: slideDefault1,
-		urlMovil: slideDefault,
+		urlMovil: slideDefault11,
 		alt: 'mas destacados'
 	},
 	{
 		url: slideDefault2,
-		urlMovil: slideDefault,
+		urlMovil: slideDefault22,
 		alt: 'mas destacados'
 	},
 	{
 		url: slideDefault3,
-		urlMovil: slideDefault,
+		urlMovil: slideDefault33,
 		alt: 'mas destacados'
 	},
 	{
 		url: slideDefault4,
-		urlMovil: slideDefault,
+		urlMovil: slideDefault44,
 		alt: 'mas destacados'
 	},
 	{
 		url: slideDefault5,
-		urlMovil: slideDefault,
+		urlMovil: slideDefault11,
 		alt: 'mas destacados'
 	},
 	{
 		url: slideDefault6,
-		urlMovil: slideDefault,
+		urlMovil: slideDefault66,
 		alt: 'mas destacados'
 	}
 ]
