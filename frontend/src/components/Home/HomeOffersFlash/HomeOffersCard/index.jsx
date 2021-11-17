834a1280501c97d6a45ab5398bb667113d733b4c
Camiloatalaya <developeratalaya2@outlook.com>
2021-11-17 08:28:04 -0500

    refactoriznado el codigo de hoteles

--- frontend/src/components/Home/HomeOffersFlash/HomeOffersCard/index.jsx
+++ frontend/src/components/Home/HomeOffersFlash/HomeOffersCard/index.jsx
@@ -4,7 +4,7 @@ import { CardStyle } from './styles'
 export const HomeOffersCard = ({ image, title, cop }) => {
 	const handleBooking = () => {
 		window.open(
-			`https://wa.me/573205701705?text=Me%20interesa%20ver%20la%20oferta%20flash%20${title}.`
+			`https://wa.me/573205702937?text=Me%20interesa%20ver%20la%20oferta%20flash%20${title}.`
 		)
 	}
 
